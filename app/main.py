b9f0a901cb70449057300732d24a54b44074caac
freddy <freddy@operaciones.europe-west1-b.c.trans-engine-290916.internal>
2020-11-30 17:50:33 +0000

    myapp Ampliacion de operativos

---
+++ app/main.py
@@ -0,0 +1,19 @@
+from flask import jsonify
+from flask import Flask
+app = Flask(__name__)
+
+@app.route('/hello')
+def hello_world():
+    message =  "Hola mundo, soy Python!"
+    response = {
+            "message": message,
+            "lenght": len(message)
+            }
+    return jsonify(response)
+
+@app.route('/bye')
+def bye_world():
+    return ("Adios mundo cruel!")
+
+if __name__ == '__main__':
+    app.run(debug=True, host='0.0.0.0')
